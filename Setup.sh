20cb269a8cff5c16567e985db387201c48734e03
LGM-TheCakeKing <69428980+LGM-TheCakeKing@users.noreply.github.com>
2021-12-03 13:07:04 +0100

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,7 @@
+mv RaspberryBinaryClock/clock.py /home/
+mv RaspberryBinaryClock/clock.service /lib/systemd/system/
+sudo chmod 644 /lib/systemd/system/clock.service
+chmod +x /home/clock.py
+sudo systemctl daemon-reload
+sudo systemctl enable clock.service
+sudo systemctl start clock.service
